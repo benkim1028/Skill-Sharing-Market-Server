02081567d48180b21edbcba70f806ef0b26c0011
Ben Kim <benkim1028@gmail.com>
2018-01-15 23:22:47 +0000

    Update signupEndpoint.java

--- src/main/java/skillbackend/Endpoints/signupEndpoint.java
+++ src/main/java/skillbackend/Endpoints/signupEndpoint.java
@@ -1,42 +1,43 @@
-//package skillbackend.Endpoints;
-//
-//import javassist.bytecode.stackmap.TypeData;
-//import org.mindrot.jbcrypt.BCrypt;
-//import skillbackend.Model.Credentials;
-//
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.core.MediaType;
-//import javax.ws.rs.core.Response;
-//import java.util.logging.Level;
-//import java.util.logging.Logger;
-//
-//@Path("/signup")
-//public class signupEndpoint{
-//    private static final Logger LOGGER = Logger.getLogger( TypeData.ClassName.class.getName() );
-//
-//    @POST
-//    @Produces(MediaType.APPLICATION_JSON)
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public Response signup(Credentials credentials){
-//        String username = credentials.getUsername();
-//        String password = credentials.getPassword();
-//        LOGGER.log(Level.INFO, "username = " + username + ", password = " + password);
-//
-//        // get hash of both and then save into the database
-//
-//        try(
-//            return Response.ok(token).build();
-//        } catch (Exception e) {
-//            LOGGER.log(Level.SEVERE, e.toString(), e);
-//            throw e;
-//        }
-//        return Response.Status(Response.Status.FORBIDDEN).build();
-//
-//    }
-//
-//
-//
-//}
+package skillbackend.Endpoints;
+
+import javassist.bytecode.stackmap.TypeData;
+import org.mindrot.jbcrypt.BCrypt;
+import skillbackend.Model.Credentials;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Path("/signup")
+public class signupEndpoint{
+   private static final Logger LOGGER = Logger.getLogger( TypeData.ClassName.class.getName() );
+
+   @POST
+   @Produces(MediaType.APPLICATION_JSON)
+   @Consumes(MediaType.APPLICATION_JSON)
+   public Response signup(Credentials credentials){
+       String username = credentials.getUsername();
+       String password = credentials.getPassword();
+       LOGGER.log(Level.INFO, "username = " + username + ", password = " + password);
+
+       //TODO: create user table in a database
+       //TODO: get hash of password and then save userid and hash of password into the database
+
+       try(
+           return Response.ok(token).build();
+       } catch (Exception e) {
+           LOGGER.log(Level.SEVERE, e.toString(), e);
+           throw e;
+       }
+       return Response.Status(Response.Status.FORBIDDEN).build();
+
+   }
+
+
+
+}
