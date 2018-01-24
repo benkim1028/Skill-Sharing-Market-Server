db3c79115fd264ed1353257249fe8114e81c408f
Ben Kim <benkim1028@gmail.com>
2018-01-24 08:52:50 -0800

    MongoDB Connection

--- src/main/java/skillbackend/Database/MongoDB.java
+++ src/main/java/skillbackend/Database/MongoDB.java
@@ -10,10 +10,11 @@ public class MongoDB {
     private static MongoDB single_instance = null;
     private MongoClientURI uri;
     private MongoClient mongoClient;
+    private String username = "skill";
+    private String password = "x4R2cHYkhLp16n8U"
 
     private MongoDB(){
-        uri = new MongoClientURI(
-                "mongodb+srv://skill:x4R2cHYkhLp16n8U@cluster0-0dasm.mongodb.net/");
+        uri = new MongoClientURI("mongodb+srv://" + username + ":" + password + "@cluster0-0dasm.mongodb.net/");
         mongoClient = new MongoClient(uri);
     }
 
@@ -22,26 +23,6 @@ public class MongoDB {
             return new MongoDB();
         return single_instance;
     }
-
-    static public void main(String args[]){
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://skill:x4R2cHYkhLp16n8U@cluster0-0dasm.mongodb.net/");
-
-        MongoClient mongoClient = new MongoClient(uri);
-        MongoDatabase db = mongoClient.getDatabase("skillDB");
-        System.out.println("Successfully connected");
-        //db.createCollection("user");
-        System.out.println("created");
-        MongoCollection<Document> collection = db.getCollection("user");
-        Document doc = new Document("username", "MongoDB")
-                .append("type", "database")
-                .append("count", 1)
-                .append("info", new Document("x", 203).append("y", 102));
-        collection.insertOne(doc);
-        System.out.println("added");
-
-    }
-
 }
 /*
 MongoDB Compass Login
