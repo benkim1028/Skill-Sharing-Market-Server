57a4a75788170de2cc7cfb6a5191c524d10607ea
Ben <benkim1028@gmail.com>
2018-01-23 21:46:19 -0800

    MongoDB

--- src/main/java/skillbackend/Database/MongoDB.java
+++ src/main/java/skillbackend/Database/MongoDB.java
@@ -2,14 +2,49 @@ package skillbackend.Database;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class MongoDB {
+    private static MongoDB single_instance = null;
+    private MongoClientURI uri;
+    private MongoClient mongoClient;
 
-    private static MongoClientURI uri = new MongoClientURI("mongodb+srv://skill:XRQuvlG1Qe0Oy4cS@cluster0.mongodb.net/");
-    private static MongoClient mongoClient = new MongoClient(uri);
-    private static MongoDatabase database = mongoClient.getDatabase("test");
-//    public static void main (String[] args){
-//        System.out.println(database);
-//    }
+    private MongoDB(){
+        uri = new MongoClientURI(
+                "mongodb+srv://skill:x4R2cHYkhLp16n8U@cluster0-0dasm.mongodb.net/");
+        mongoClient = new MongoClient(uri);
+    }
+
+    public static MongoDB getInstance(){
+        if(single_instance == null)
+            return new MongoDB();
+        return single_instance;
+    }
+
+    static public void main(String args[]){
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb+srv://skill:x4R2cHYkhLp16n8U@cluster0-0dasm.mongodb.net/");
+
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase db = mongoClient.getDatabase("skillDB");
+        System.out.println("Successfully connected");
+        //db.createCollection("user");
+        System.out.println("created");
+        MongoCollection<Document> collection = db.getCollection("user");
+        Document doc = new Document("username", "MongoDB")
+                .append("type", "database")
+                .append("count", 1)
+                .append("info", new Document("x", 203).append("y", 102));
+        collection.insertOne(doc);
+        System.out.println("added");
+
+    }
+
 }
+/*
+MongoDB Compass Login
+ID: SkillDev
+Password: 싸랑한다1! (영어로)
+ */
