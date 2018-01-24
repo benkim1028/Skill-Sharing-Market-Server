113a6ad1e10136a976a00f89c0bd52303b52fb88
Ben Kim <benkim1028@gmail.com>
2018-01-24 09:53:24 -0800

    Update MongoDB.java

--- src/main/java/skillbackend/Database/MongoDB.java
+++ src/main/java/skillbackend/Database/MongoDB.java
@@ -10,18 +10,24 @@ public class MongoDB {
     private static MongoDB single_instance = null;
     private MongoClientURI uri;
     private MongoClient mongoClient;
+    private MongoDatabase db;
     private String username = "skill";
     private String password = "x4R2cHYkhLp16n8U"
 
     private MongoDB(){
         uri = new MongoClientURI("mongodb+srv://" + username + ":" + password + "@cluster0-0dasm.mongodb.net/");
         mongoClient = new MongoClient(uri);
+        db = mongoClient.getDatabase("skillDB");
     }
 
     public static MongoDB getInstance(){
         if(single_instance == null)
             return new MongoDB();
         return single_instance;
+    }
+    
+    public MongoDatabase getDB(){
+        return db;
     }
 }
 /*
