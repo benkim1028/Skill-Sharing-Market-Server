6913cbb1239498e2c53bc5e5c7821d1ae512768f
Ben Kim <benkim1028@gmail.com>
2018-10-08 12:02:34 -0700

    database fix

--- src/main/java/skillbackend/Database/MongoDB.java
+++ src/main/java/skillbackend/Database/MongoDB.java
@@ -25,8 +25,8 @@ public class MongoDB {
         options_builder.maxConnectionIdleTime(100000);
         MongoClientURI uri = new MongoClientURI(
                 "mongodb://"+username+":"+password+"@ds231739.mlab.com:31739/heroku_6l526rzg", options_builder);
-        //MongoClient mongoClient = new MongoClient(uri);
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+        MongoClient mongoClient = new MongoClient(uri);
+        //MongoClient mongoClient = new MongoClient("localhost", 27017);
         db = mongoClient.getDatabase(uri.getDatabase());
 
     }
